4bc3564a5c6bce0179e9ebe1ed779cd816511ea8
Yulia Vasileva <67293591+teploletom@users.noreply.github.com>
2022-12-26 05:32:19 +0300

    Add files via upload

--- HW12.py
+++ HW12.py
@@ -3,6 +3,7 @@ import pytest
 from selenium import webdriver
 import time
 from random import randint
+from selenium.webdriver.common.by import By
 
 
 
@@ -23,9 +24,10 @@ def test_example(driver):
     list = []
 
     ###General tab
-    driver.find_element_by_css_selector("#tab-general > table > tbody > tr:nth-child(1) > td > label:nth-child(3) > input[type=radio]").click()
-    driver.find_element_by_xpath("/html/body/div[2]/div/div/table/tbody/tr/td[3]/form/div/div/div[1]/table/tbody/tr[1]/td/label[1]/input")
-    driver.find_element_by_name("name[en]").send_keys("Gold Duck8")
+    driver.find_element_by_name("status").click()
+    random_num = str(randint(0, 9999))
+    new_duck = "Gold Duck" + random_num
+    driver.find_element_by_name("name[en]").send_keys(new_duck)
     driver.find_element_by_name("code").send_keys("11118")
     driver.find_element_by_css_selector("#tab-general > table > tbody > tr:nth-child(4) > td > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > input[type=checkbox]").click()
     driver.find_element_by_css_selector("#tab-general > table > tbody > tr:nth-child(4) > td > div > table > tbody > tr:nth-child(2) > td:nth-child(1) > input[type=checkbox]").click()
@@ -34,27 +36,23 @@ def test_example(driver):
     driver.find_element_by_name("new_images[]").send_keys(os.getcwd() + "\duck.jpg")
     driver.find_element_by_name("date_valid_from").send_keys("20122022")
     driver.find_element_by_name("date_valid_to").send_keys("20122023")
-    time.sleep(10)
 
     ### Information tab
-    driver.find_element_by_xpath("/html/body/div[2]/div/div/table/tbody/tr/td[3]/form/div/ul/li[2]/a").click()
-    time.sleep(10)
-    driver.find_element_by_xpath("/html/body/div[2]/div/div/table/tbody/tr/td[3]/form/div/div/div[2]/table/tbody/tr[1]/td/select/option[2]").click()
+    driver.find_element(By.LINK_TEXT, "Information").click()
+    driver.find_element_by_css_selector("#tab-information > table > tbody > tr:nth-child(1) > td > select > option:nth-child(2)").click()
     driver.find_element_by_name("keywords").send_keys("Duck")
     driver.find_element_by_name("short_description[en]").send_keys("short_description")
     driver.find_element_by_css_selector("div.trumbowyg-editor").send_keys("Description")
     driver.find_element_by_name("head_title[en]").send_keys("Title")
     driver.find_element_by_name("meta_description[en]").send_keys("Meta_data")
-    driver.find_element_by_xpath("/html/body/div[2]/div/div/table/tbody/tr/td[3]/form/div/ul/li[4]/a").click()
-    time.sleep(10)
 
     ###Prices
+    driver.find_element(By.LINK_TEXT, "Prices").click()
     driver.find_element_by_name("purchase_price").send_keys("20")
     driver.find_element_by_css_selector("#tab-prices > table:nth-child(2) > tbody > tr > td > select > option:nth-child(2)").click()
     driver.find_element_by_name("prices[USD]").send_keys("20")
     driver.find_element_by_name("prices[EUR]").send_keys("23")
     driver.find_element_by_name("save").click()
-    time.sleep(10)
 
     ### check out the new Duck in Catalog
     m = len(driver.find_elements_by_css_selector("tr.row"))  # count of all elements
@@ -62,9 +60,9 @@ def test_example(driver):
         ducks_name = driver.find_elements_by_css_selector(f'#content > form > table > tbody > tr:nth-child({i}) > td:nth-child(3) > a')
         for y in ducks_name:
             y = y.text
-            if y == "Gold Duck8":
-                print("The Gold Duck8 was added")
-    time.sleep(10)
+            if y == new_duck:
+                print(new_duck + " was added")
+
 
 
 
