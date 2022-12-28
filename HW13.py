8368d16021dcd0691f4688b20e2bac72e2ced072
Yulia Vasileva <67293591+teploletom@users.noreply.github.com>
2022-12-29 02:16:03 +0300

    Add files via upload

--- HW13.py
+++ HW13.py
@@ -25,14 +25,36 @@ def test_example(driver):
 
     # Go to cart
     driver.get("http://localhost/litecart/en/checkout")
-    time.sleep(10)
-    driver.find_element_by_css_selector("#box-checkout-cart > div > ul > li:nth-child(2)").click()
     m = len(driver.find_elements_by_css_selector("#order_confirmation-wrapper > table > tbody td.item"))
-    print(m)
-    while m >= 1:
-        print(m)
-        driver.find_element_by_name("remove_cart_item").click()
-        m = m - 1
-    t = WebDriverWait(driver, 20)
-    a = t.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#checkout-cart-wrapper > p:nth-child(1) > em"))).get_attribute("textContent")
-    assert a == "There are no items in your cart.", "Not sorted"
+    driver.find_element_by_css_selector("#box-checkout-cart > ul > li:nth-child(1) > a").click()  # stop karusel
+    driver.find_element_by_name("remove_cart_item").click()
+
+    for i in (1, m + 1):
+        if len(driver.find_elements_by_css_selector("#order_confirmation-wrapper > table > tbody td.item")) != 0:
+            WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div [id=checkout-cart-wrapper][style="opacity: 1;"]')))
+            driver.find_element_by_name("remove_cart_item").click()
+
+    a = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#checkout-cart-wrapper > p:nth-child(1) > em"))).get_attribute("textContent")
+    assert a == "There are no items in your cart.", "Not deleted item(s)"
+    time.sleep(20)
+
+    # t1.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#box-checkout-cart > div > ul > li:nth-child(1) > form > div > p:nth-child(4) > button")))
+    # driver.find_element_by_css_selector("#box-checkout-cart > div > ul > li:nth-child(1) > form > div > p:nth-child(4) > button").click()
+    # f = driver.find_elements_by_css_selector("td.item").size()
+    #  if WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_css_selector("td.item").is_displayed()):
+
+  #      m = m - 1
+
+        # driver.find_element_by_name("remove_cart_item").click()
+
+ #    driver.find_element_by_css_selector("#box-checkout-cart > div > ul > li:nth-child(1) > form > div > p:nth-child(4) > button").click()
+ #  #  time.sleep(30)
+ #    driver.find_element_by_css_selector("#box-checkout-cart > div > ul > li:nth-child(1) > form > div > p:nth-child(4) > button").click()
+ # #   time.sleep(30)
+ #    driver.find_element_by_css_selector("#box-checkout-cart > div > ul > li:nth-child(1) > form > div > p:nth-child(4) > button").click()
+ #      #  m = m - 1
+ #    #
+
+    # t3 = WebDriverWait(driver, 20)
+    # a = t3.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#checkout-cart-wrapper > p:nth-child(1) > em"))).get_attribute("textContent")
+    # assert a == "There are no items in your cart.", "Not sorted"
