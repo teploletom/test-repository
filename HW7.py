c583e4ce60e53bf11e4671f7e96e905ce1fdee29
Yulia Vasileva <67293591+teploletom@users.noreply.github.com>
2022-12-30 16:02:25 +0300

    Update HW7.py

--- HW7.py
+++ HW7.py
@@ -1,9 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+
 
 
 @pytest.fixture
